56648b95f8842e54c30a6f39ab61e92af0adb378
Fernando Lima <silvanandolima@hotmail.com>
2023-10-05 22:09:37 -0300

    atualização perfil

--- src/main/java/com/itb/mif3an/academicologin/model/Endereco.java
+++ src/main/java/com/itb/mif3an/academicologin/model/Endereco.java
@@ -1,5 +1,7 @@
 package com.itb.mif3an.academicologin.model;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -68,6 +70,21 @@ public class Endereco {
 	}
 	public void setUser(User user) {
 		this.user = user;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(id, other.id);
 	}
 	
    
